35c547b307a5d0f713fffdafdf59aad30c6533ce
A-Dhanush <119718907+A-Dhanush@users.noreply.github.com>
2023-10-10 14:51:18 +0530

    Add files via upload

---
+++ invictus.py
@@ -0,0 +1,59 @@
+import subprocess
+import requests
+import json
+import re
+
+# Function to remove ANSI escape codes from text
+def remove_ansi_escape_codes(text):
+    ansi_escape = re.compile(r'\x1B(?:[@-Z\\-_]|\[[0-?]*[ -/]*[@-~])')
+    return ansi_escape.sub('', text)
+
+
+def cms_find(param, key):
+    url = f'https://whatcms.org/APIEndpoint/Technology?key={key}&url={param}'
+    r = requests.get(url)
+    var = r.json()
+    
+    if 'results' in var and var['results']:
+        cms_name = var['results'][0]['name']
+
+    else:
+        cms_name = None 
+    
+    return cms_name
+
+def cms_scan(cms_name, param):
+    sanitized_param = "".join(c for c in param if c.isalnum() or c in ('-', '_'))
+    file_path = f"reports/cms_scan_{sanitized_param}.txt"
+
+    
+    with open(file_path, "w") as f:
+        cms = (f"CMS Detected : {cms_name}")
+        f.write(cms)
+        if cms_name == "WordPress":
+            print ("!---Starting WordPress Scan---!")
+            scan_output = subprocess.check_output(["wpscan", "--url", param, "--no-banner", "--random-user-agent", "aggressive"], stderr=subprocess.STDOUT)
+        elif cms_name == "Joomla":
+            print ("!---Starting Joomla Scan---!")
+            scan_output = subprocess.check_output(["perl", "joomscan.pl", "-u", param], cwd="/Users/dhanush/Downloads/Project/Tools/joomscan", stderr=subprocess.STDOUT)
+        elif cms_name == "Drupal":
+            print ("!---Starting Drupal Scan---!")
+            scan_output = subprocess.check_output(["droopescan", "scan", "drupal", "-u", param], stderr=subprocess.STDOUT)
+        else:
+            no_supp = f"No support for {cms_name} yet."
+            f.write(no_supp)
+
+        # Remove ANSI escape codes from the scan output
+        scan_output = remove_ansi_escape_codes(scan_output.decode('utf-8'))
+
+        # Write the plain text scan results to the file
+        f.write(scan_output)
+
+
+    # Read and return the scan results from the file
+    with open(file_path, "r") as f:
+        scan_results = f.read()
+
+    print("Scan Results:")
+    print(scan_results)  # Add this line for debugging
+    return scan_results
