406d05fde27943fa5de6483d96f1917306cabd3b
A-Dhanush <119718907+A-Dhanush@users.noreply.github.com>
2023-11-08 12:29:17 +0530

    Update invictus.py

--- invictus.py
+++ invictus.py
@@ -1,6 +1,7 @@
 import subprocess
 import requests
 import json
+import os
 import re
 
 # Function to remove ANSI escape codes from text
@@ -16,44 +17,61 @@ def cms_find(param, key):
     
     if 'results' in var and var['results']:
         cms_name = var['results'][0]['name']
-
     else:
-        cms_name = None 
-    
+        cms_name = "No support for CMS"
+
     return cms_name
 
 def cms_scan(cms_name, param):
     sanitized_param = "".join(c for c in param if c.isalnum() or c in ('-', '_'))
-    file_path = f"reports/cms_scan_{sanitized_param}.txt"
-
+    file_path = os.path.join(os.path.dirname(__file__), 'reports', f"cms_scan_{sanitized_param}.txt")
     
-    with open(file_path, "w") as f:
-        cms = (f"CMS Detected : {cms_name}")
-        f.write(cms)
-        if cms_name == "WordPress":
-            print ("!---Starting WordPress Scan---!")
-            scan_output = subprocess.check_output(["wpscan", "--url", param, "--no-banner", "--random-user-agent", "aggressive"], stderr=subprocess.STDOUT)
-        elif cms_name == "Joomla":
-            print ("!---Starting Joomla Scan---!")
-            scan_output = subprocess.check_output(["perl", "joomscan.pl", "-u", param], cwd="/Users/dhanush/Downloads/Project/Tools/joomscan", stderr=subprocess.STDOUT)
-        elif cms_name == "Drupal":
-            print ("!---Starting Drupal Scan---!")
-            scan_output = subprocess.check_output(["droopescan", "scan", "drupal", "-u", param], stderr=subprocess.STDOUT)
+    scan_output = ""
+
+    try:
+        if cms_name:
+            cms = f"CMS Detected: {cms_name}"
+            with open(file_path, "w") as f:
+                f.write(cms)
+            if cms_name == "WordPress":
+                print ("!---Starting WordPress Scan---!")
+                scan_output = subprocess.check_output(["wpscan", "--url", param, "--no-banner", "--random-user-agent", "aggressive"], stderr=subprocess.STDOUT)
+            elif cms_name == "Joomla":
+                print ("!---Starting Joomla Scan---!")
+                scan_output = subprocess.check_output(["perl", "joomscan.pl", "-u", param], cwd="/Users/dhanush/Downloads/Project/Tools/joomscan", stderr=subprocess.STDOUT)
+            elif cms_name == "Drupal":
+                print ("!---Starting Drupal Scan---!")
+                scan_output = subprocess.check_output(["droopescan", "scan", "drupal", "-u", param], stderr=subprocess.STDOUT)
+            else:
+                no_supp = "No support for CMS yet."
+                with open(file_path, "a") as f:
+                    f.write(no_supp)
+                return no_supp
         else:
-            no_supp = f"No support for {cms_name} yet."
-            f.write(no_supp)
+            no_supp = "No support for CMS yet."
+            with open(file_path, "w") as f:
+                f.write(no_supp)
+            return no_supp
+
+        # Check if scan_output is bytes and convert it to a string
+        if isinstance(scan_output, bytes):
+            scan_output = scan_output.decode('utf-8')
 
         # Remove ANSI escape codes from the scan output
-        scan_output = remove_ansi_escape_codes(scan_output.decode('utf-8'))
+        scan_output = remove_ansi_escape_codes(scan_output)
 
-        # Write the plain text scan results to the file
-        f.write(scan_output)
+        with open(file_path, "a") as f:
+            # Write the plain text scan results to the file
+            f.write(scan_output)
 
+        # Read and return the scan results from the file
+        with open(file_path, "r") as f:
+            scan_results = f.read()
 
-    # Read and return the scan results from the file
-    with open(file_path, "r") as f:
-        scan_results = f.read()
-
-    print("Scan Results:")
-    print(scan_results)  # Add this line for debugging
-    return scan_results
+        print("Scan Results:")
+        print(scan_results)  # Add this line for debugging
+        return scan_results
+    except Exception as e:
+        # Handle exceptions gracefully and log the error
+        print(f"Error: {str(e)}")
+        return str(e)
